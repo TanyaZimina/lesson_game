11e29f158254bb0fff8b5c3e2b3a665b065abbc1
Tatiana Zimina <tany-zimina@mail.ru>
2021-06-13 14:11:37 +0300

    lesson06

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function check(){
+    let number = Math.floor((Math.random() * 100) + 1); //число которое загадывает программа
+console.log(number);
+
+    function game(){
+      let userInput = prompt('введите число');
+      if(userInput == null) return alert('игра окончена');
+      if(isNaN(parseFloat(userInput)) || !isFinite(userInput)) prompt('вы ввели не число');
+      if(userInput > number) alert('ваше число больше');
+      if(userInput < number) alert('ваше число меньше');
+      if(userInput == number) return alert('вы угадали');
+      game();
+    }
+    game();
+  };
+  check();
+  console.dir(check);
